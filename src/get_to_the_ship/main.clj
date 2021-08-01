a842b05455b6776e03171c71f0b2414f72d4c688
get-to-the-ship <get-to-the-ship@program>
2021-08-01 20:27:13 +0300

    i add canvas

--- src/get_to_the_ship/main.clj
+++ src/get_to_the_ship/main.clj
@@ -13,12 +13,31 @@
    (javafx.event Event EventHandler)
    (javafx.stage WindowEvent)
    (javafx.scene.control DialogEvent Dialog ButtonType ButtonBar$ButtonData)
+   (javafx.scene.canvas Canvas)
+   (javafx.scene.paint Color)
    #_javafx.application.Platform))
 
 (println "clojure.compiler.direct-linking" (System/getProperty "clojure.compiler.direct-linking"))
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true) (clojure.spec.alpha/check-asserts true))
 
-(defn stage
+(defn fx-canvas
+  [{:as opts
+    :keys [::canvas-width
+           ::canvas-height]}]
+  {:fx/type :canvas
+   :width canvas-width
+   :height canvas-height
+   :draw (fn [^Canvas canvas]
+           (doto (.getGraphicsContext2D canvas)
+             (.clearRect 0 0 canvas-width canvas-height)
+             (.setStroke Color/LIGHTGREY)
+             (.strokeRect 0 0 canvas-width canvas-height)
+             #_(.setFill Color/LIGHTGREY)
+             #_(.fillRoundRect 0 0 canvas-width canvas-height canvas-height canvas-height)
+             #_(.setFill Color/GREEN)
+             #_(.fillRoundRect 0 0 (* canvas-width 10) canvas-height canvas-height canvas-height)))})
+
+(defn fx-stage
   [{:as opts
     :keys [::program-name]}]
   (println ::opts opts)
@@ -28,19 +47,22 @@
    :width 1600
    :height 1200
    :scene {:fx/type :scene
-           :root {:fx/type :h-box
-                  :children [#_{:fx/type :label :text program-name}]}}})
+           :root {:fx/type :v-box
+                  ;; :padding 100
+                  :children [{:fx/type fx-canvas
+                              ::canvas-width 1400
+                              ::canvas-height 1000}]}}})
 
 (defonce stateA (atom nil))
 
 (defn -main [& args]
   (println ::-main)
   (let [renderer (cljfx.api/create-renderer)]
-    (reset! stateA {:fx/type stage
+    (reset! stateA {:fx/type fx-stage
                     ::program-name "get-to-the-ship"
                     ::renderer renderer})
     (add-watch stateA :watch-fn (fn [k stateA old-state new-state] (-> new-state
-                                                                       (assoc :fx/type stage)
+                                                                       (assoc :fx/type fx-stage)
                                                                        (renderer))))
 
     (javafx.application.Platform/setImplicitExit true)
