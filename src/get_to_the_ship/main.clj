89e9cfbe6d6913e6f3518079c7f1b8a2854e853c
get-to-the-ship <get-to-the-ship@program>
2021-08-02 12:27:24 +0300

    i remove javafx

--- src/get_to_the_ship/main.clj
+++ src/get_to_the_ship/main.clj
@@ -7,26 +7,10 @@
                                      pipeline pipeline-async]]
    [clojure.string]
    [clojure.spec.alpha :as s]
-   [clojure.java.io :as io])
-  (:import
-   (javafx.event Event EventHandler)
-   (javafx.stage WindowEvent)
-   (javafx.scene.control DialogEvent Dialog ButtonType ButtonBar$ButtonData)
-   (javafx.scene.canvas Canvas)
-   (javafx.scene.paint Color)
-   #_javafx.application.Platform))
+   [clojure.java.io :as io]))
 
 (println "clojure.compiler.direct-linking" (System/getProperty "clojure.compiler.direct-linking"))
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true) (clojure.spec.alpha/check-asserts true))
-
-#_(doto (.getGraphicsContext2D canvas)
-    (.clearRect 0 0 canvas-width canvas-height)
-    (.setStroke Color/LIGHTGREY)
-    (.strokeRect 0 0 canvas-width canvas-height)
-    #_(.setFill Color/LIGHTGREY)
-    #_(.fillRoundRect 0 0 canvas-width canvas-height canvas-height canvas-height)
-    #_(.setFill Color/GREEN)
-    #_(.fillRoundRect 0 0 (* canvas-width 10) canvas-height canvas-height canvas-height))
 
 (defonce stateA (atom nil))
 
@@ -35,8 +19,6 @@
   (let []
     (reset! stateA {::program-name "get-to-the-ship"})
     (add-watch stateA :watch-fn (fn [k stateA old-state new-state] new-state))
-
-    (javafx.application.Platform/setImplicitExit true)
 
     (go)))
 
