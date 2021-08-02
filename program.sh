145100e2fa96cb242307c594100604423db31677
get-to-the-ship <get-to-the-ship@program>
2021-08-02 21:42:52 +0300

    i use new commit of deps-repl - now with clj -X:repl

--- program.sh
+++ program.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 repl(){
-  clj -M:repl
+  clj -X:repl \
+    deps-repl.core/process \
+    :main-ns get-to-the-ship.main \
+    :port 7788 \
+    :host '"0.0.0.0"' \
+    :repl? true \
+    :nrepl? false
 }
 
 main(){
