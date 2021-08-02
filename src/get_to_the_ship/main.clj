b59d777dddd1b4fd964b6fdcc6427238392c44a9
get-to-the-ship <get-to-the-ship@program>
2021-08-02 15:36:21 +0300

    i add canvas, use later-var-root to set vars for repl

--- src/get_to_the_ship/main.clj
+++ src/get_to_the_ship/main.clj
@@ -10,12 +10,15 @@
    [clojure.java.io :as io])
   (:import
    (javax.swing JFrame JLabel JButton SwingConstants JMenuBar JMenu)
+   (java.awt Canvas)
    (java.awt.event WindowListener)))
 
 (println "clojure.compiler.direct-linking" (System/getProperty "clojure.compiler.direct-linking"))
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true) (clojure.spec.alpha/check-asserts true))
 
 (defonce stateA (atom nil))
+(defonce jframe nil)
+(defonce canvas nil)
 
 (defn -main [& args]
   (println ::-main)
@@ -23,14 +26,17 @@
     (reset! stateA {})
     (add-watch stateA :watch-fn (fn [k stateA old-state new-state] new-state))
 
-    (let [jframe (JFrame. "get-to-the-ship")]
-
+    (let [jframe (JFrame. "get-to-the-ship")
+          canvas (Canvas.)]
+      (alter-var-root #'get-to-the-ship.main/jframe (constantly jframe))
+      (alter-var-root #'get-to-the-ship.main/canvas (constantly canvas))
       (doto jframe
         (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
         (.setSize 1600 1200)
         (.setLocationByPlatform true)
-        (.setVisible true)))
-
+        (.setVisible true)
+        (.add (doto canvas
+                (.setSize 1600 1200)))))
     (go)))
 
 (comment
