ddbd4772a5fdbf13f01470f244b26735bdd6a082
get-to-the-ship <get-to-the-ship@program>
2021-08-03 14:01:53 +0300

    i revert "i remove spec library i'm not using"
    This reverts commit 0dff8496a49306bfb41c1cfdcba840e811aa82cf.

--- src/get_to_the_ship/main.clj
+++ src/get_to_the_ship/main.clj
@@ -6,6 +6,7 @@
                                      timeout to-chan  sliding-buffer dropping-buffer
                                      pipeline pipeline-async]]
    [clojure.string]
+   [clojure.spec.alpha :as s]
    [clojure.java.io :as io])
   (:import
    (javax.swing JFrame JLabel JButton SwingConstants JMenuBar JMenu JTextArea)
@@ -13,7 +14,7 @@
    (java.awt.event WindowListener KeyListener KeyEvent)))
 
 (println "clojure.compiler.direct-linking" (System/getProperty "clojure.compiler.direct-linking"))
-(do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
+(do (set! *warn-on-reflection* true) (set! *unchecked-math* true) (clojure.spec.alpha/check-asserts true))
 
 (defonce stateA (atom nil))
 (defonce ^JFrame jframe nil)
