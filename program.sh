3c2d29a362c560d407d80474aeb2071397bf930b
get-to-the-ship <get-to-the-ship@program>
2021-09-01 17:56:10 +0300

    i use ripley library to open repl

--- program.sh
+++ program.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 repl(){
-  
+  clj \
+    -J-Dclojure.core.async.pool-size=1 \
+    -X:repl ripley.core/process \
+    :main-ns get-to-the-ship.main
 }
 
 main(){
-
+  echo 1
 }
 
 uberjar(){
-
+  echo 1
 }
 
 release(){
-  
+  echo 1
 }
 
 "$@"
