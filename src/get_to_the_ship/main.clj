761b5629ddb4059af373114af8d8b047f1dd1a0b
get-to-the-ship <get-to-the-ship@program>
2021-09-01 17:54:28 +0300

    i add main

---
+++ src/get_to_the_ship/main.clj
@@ -0,0 +1,24 @@
+(ns get-to-the-ship.main
+  (:require 
+    [clojure.core.async :as a :refer [<! >! <!! >!! chan put! take! go alt! alts! do-alts close! timeout pipe mult tap untap 
+                                      pub sub unsub mix admix unmix dropping-buffer sliding-buffer pipeline pipeline-async to-chan! thread]]
+    [clojure.string]
+    [clojure.java.io :as io]))
+
+#_(println (System/getProperty "clojure.core.async.pool-size"))
+(do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
+
+(defn window
+  []
+)
+
+(defn reload
+  []
+)
+
+(defn -main
+  [& args]
+  (let []
+
+  )
+)
