3c99a5aa220b01e8acc4c3e3dc169a52cd57a509
get-to-the-ship <get-to-the-ship@program>
2021-09-01 18:08:47 +0300

    i use swing library and show window

--- src/get_to_the_ship/main.clj
+++ src/get_to_the_ship/main.clj
@@ -3,25 +3,47 @@
     [clojure.core.async :as a :refer [<! >! <!! >!! chan put! take! go alt! alts! do-alts close! timeout pipe mult tap untap 
                                       pub sub unsub mix admix unmix dropping-buffer sliding-buffer pipeline pipeline-async to-chan! thread]]
     [clojure.string]
-    [clojure.java.io :as io]))
+    [clojure.java.io :as io])
+  (:import
+    (javax.swing JFrame)
+  )    
+)
 
 #_(println (System/getProperty "clojure.core.async.pool-size"))
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
 
 (defonce stateA nil)
+(def ^:dynamic jframe nil)
 
 (defn window
   []
+  (let [jframe (JFrame. "i am get-to-the-ship program")]
+
+  (doto jframe
+    (.setSize 1600 1200)
+    (.setLocation 1700 300)
+    (.setVisible true)
+  )
+
+  (alter-var-root #'get-to-the-ship.main/jframe (constantly jframe))
+
+  nil
+  )
 )
 
 (defn reload
   []
+  (require 
+    '[get-to-the-ship.main]
+    :reload)
 )
 
 (defn -main
   [& args]
   (let []
     (reset! stateA {})
+
+    (window)
 
   )
 )
