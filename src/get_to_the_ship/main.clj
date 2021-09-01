93a93352a1e27c97e4d59e78a82731b45a8ad64b
get-to-the-ship <get-to-the-ship@program>
2021-09-01 23:27:03 +0300

    i add canvas and repl

--- src/get_to_the_ship/main.clj
+++ src/get_to_the_ship/main.clj
@@ -6,7 +6,8 @@
     [clojure.string]
     [clojure.java.io :as io])
   (:import
-    (javax.swing JFrame WindowConstants ImageIcon)
+    (javax.swing JFrame WindowConstants ImageIcon JTextArea JScrollPane JPanel BoxLayout)
+    (java.awt Canvas Graphics)
   )    
 )
 
@@ -14,24 +15,62 @@
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
 
 (defonce stateA (atom nil))
-(def ^:dynamic jframe nil)
+(def ^:dynamic ^JFrame jframe nil)
+(def ^:dynamic ^Canvas canvas nil)
+(def ^:dynamic ^JTextArea repl nil)
+(def ^:dynamic ^JTextArea output nil)
+(def ^:dynamic ^JScrollPane scroll-pane nil)
+(def ^:dynamic ^Graphics graphics nil)
 
 (defn window
   []
-  (let [jframe (JFrame. "i am get-to-the-ship program")]
+  (let [jframe (JFrame. "i am get-to-the-ship program")
+        canvas (Canvas.)
+        graphics (.getGraphics canvas)
+        jpanel (JPanel.)
+        repl (JTextArea. 1 100)
+        output (JTextArea. 14 100)
+        scroll-pane (JScrollPane.)
+        layout (BoxLayout. jpanel BoxLayout/Y_AXIS)]
 
   (when-let [url (io/resource "icon.png")]
     (.setIconImage jframe (.getImage (ImageIcon. url)))
   )
 
+  (doto canvas
+    (.setSize 1600 900)
+  )
+  
+  (doto output
+    (.setEditable false)
+  )
+
+  (doto scroll-pane
+    (.setViewportView output)
+  )
+
+  (doto jpanel
+    (.setLayout layout)
+    (.add canvas)
+    (.add scroll-pane)
+    (.add repl)
+    )
+
   (doto jframe
-    (.setDefaultCloseOperation WindowConstants/EXIT_ON_CLOSE)
+    (.setDefaultCloseOperation WindowConstants/DISPOSE_ON_CLOSE #_WindowConstants/EXIT_ON_CLOSE)
     (.setSize 1600 1200)
     (.setLocation 1700 300)
+    (.add jpanel)
     (.setVisible true)
   )
 
   (alter-var-root #'get-to-the-ship.main/jframe (constantly jframe))
+  (alter-var-root #'get-to-the-ship.main/canvas (constantly canvas))
+  (alter-var-root #'get-to-the-ship.main/scroll-pane (constantly scroll-pane))
+  (alter-var-root #'get-to-the-ship.main/repl (constantly repl))
+  (alter-var-root #'get-to-the-ship.main/output (constantly output))
+  (alter-var-root #'get-to-the-ship.main/graphics (constantly graphics))
+
 
   nil
   )
